a0805be6a2bc902575123573037b6c9c1c7d923e
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-04-01 13:10:52 +0200

    Remove AddThis
    And move scripts inside scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,41 @@
-jQuery(function($){
-    $('a.fancybox').fancybox();
-});
+(function($){
+  $(function(){
+    // Initialize tooltips
+    $('[data-toggle="tooltip"]')
+    .tooltip();
+
+    // Init Fancybox
+    $('a.fancybox')
+    .fancybox();
+
+    // Set current year
+    var currentDate = new Date();
+    $('#credits-year').text( currentDate.getFullYear() );
+
+    // Get package list
+    var packageList = $('#package-list');
+    $.get('/data/packages.json', function (data){
+      for ( var k in data ) {
+        packageList
+        .append(
+          $('<p>')
+          .addClass('list-group-item')
+          .append(
+            $('<label>')
+            .text( data[k].name )
+          )
+          .append(
+            $('<span>')
+            .addClass('text-muted')
+            .text( data[k].description )
+          )
+          .append(
+            $('<span>')
+            .addClass('badge')
+            .text( data[k].version )
+          )
+        );
+      }
+    });
+  });
+})(jQuery);
